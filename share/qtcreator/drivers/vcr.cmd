452a29f0df1c71961a270a87b37c655799daec27
Kwabena W. Agyeman <kwagyeman@live.com>
2016-10-30 18:29:46 -0400

    Fixed VCR installer

--- share/qtcreator/drivers/vcr.cmd
+++ share/qtcreator/drivers/vcr.cmd
@@ -9,6 +9,7 @@ exit
 
 :install_64
 if %PROCESSOR_ARCHITECTURE%==x86 goto :install_32
+%~dp0\vcredist_x86.exe /install /quiet /norestart
 %~dp0\vcredist_x64.exe /install /quiet /norestart
 exit
 
@@ -17,5 +18,6 @@ exit
 exit
 
 :install_wow
+%~dp0\vcredist_x86.exe /install /quiet /norestart
 %~dp0\vcredist_x64.exe /install /quiet /norestart
 exit
